09f39c9ead860b66da91e0886fa4e87c0868d4aa
Shirley-April <shirleyapril23@gmail.com>
2020-08-18 12:23:20 +0300

    login

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Form from "./index.jsx";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Form />
+      </div>
+    );
+  }
+}
+export default App;
---
+++ src/index.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "./App.css";
+
+class Form extends React.Component {
+  render() {
+    return (
+      <div style={{ textAlign: "center" }}>
+        <h1>Welcome back!</h1>
+        <form>
+          <p>User name</p>
+          <input type="text"></input>
+
+          <p>Password</p>
+          <input type="text"></input>
+          <br></br>
+          <button className="btn"> LOG IN</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Form;
